3ddd5b35238768bf42cdfc590925a227006af126
ilariamotta <ilaria.motta93@gmail.com>
2026-04-01 18:36:02 +0200

    feat: printed in browser data from database

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Train;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    
+    public function index() {
+        $trains = Train::all();
+        // dd($trains);
+        return view("index", compact("trains"));
+    }
 }
